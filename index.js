c60b96841a7e82a6e9aa898f09441c2abb94c76e
Cogsy <beecher.braedan@gmail.com>
2024-05-17 11:18:38 -0600

    Setup the generation and generated a ReadMe for this challenge

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide information on how your project might be used:',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide any collaborators, third party API, or guides that provided contribution to your project:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide any test instructions if applicable. If none are required, please enter " N/A ":',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('README.md has been generated 🎉')
+    );
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        const markdown = generateMarkdown(responses);
+        writeToFile('README.md', markdown);
+    });
+}
 
 // Function call to initialize app
 init();
