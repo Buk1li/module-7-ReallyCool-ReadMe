2a89bfe9347a8cf24b405a998faeaec7e4afc138
Cogsy <beecher.braedan@gmail.com>
2024-05-17 11:43:06 -0600

    Fixed an issue with a few of the licenses not rendering correctly in the ReadMe

--- README.md
+++ README.md
@@ -1,42 +1,41 @@
 # ReadMe Generator
 
-  
-  ## Description
-  
-  This application allows users to generate a README file for their projects, based on their answers to the questions provided.
-  
-  ## Table of Contents
-  
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  
-  
-  ## Installation
-  
-  Installation for Node.js and Inquirer are required for the application to function.
-  
-  ## Usage
-  
-  The application will be used to help users generate a quality README.md, by filling out a simple form.
-  
-  ## Countribution
-  
-  N/A
-  
-  ## Tests
-  
-  N/A
-  
-  
-  
-  
-  ## Questions
 
-  If you have any questions about the project, feel free to contact me:
 
-  - GitHub: [Buk1li](https://github.com/Buk1li)
-  - Email: beecher.braedan@gmail.com
-      
+## Description
+
+This application allows users to generate a README file for their projects, based on their answers to the questions provided.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+Installation for Node.js and Inquirer are required for the application to function.
+
+## Usage
+
+The application will be used to help users generate a quality README.md, by filling out a simple form.
+
+## Contributing
+
+undefined
+
+## Tests
+
+N/A
+
+
+
+## Questions
+
+If you have any questions about the project, please feel free to contact me:
+
+- GitHub: [Buk1li](https://github.com/Buk1li)
+- Email: beecher.braedan@gmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,23 @@
+// generateMarkdown.js
+
+// Function to format the license name for the badge URL
+function formatLicenseName(license) {
+  const licenseMap = {
+    'MIT': 'MIT',
+    'GPLv3': 'GPL_3.0',
+    'Apache 2.0': 'Apache_2.0',
+    'BSD 3-Clause': 'BSD_3--Clause',
+    'None': ''
+  };
+  return licenseMap[license] || '';
+}
+
 // Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === 'None') return '';
-  return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  const formattedLicense = formatLicenseName(license);
+  if (!formattedLicense) return '';
+  return `![License](https://img.shields.io/badge/license-${formattedLicense}-blue.svg)`;
 }
 
 // Function that returns the license link
@@ -17,54 +32,54 @@ function renderLicenseLink(license) {
 function renderLicenseSection(license) {
   if (license === 'None') return '';
   return `## License
-  
-  This project is licensed under the ${license} license.`
+
+This project is licensed under the ${license} license.`;
 }
 
 // Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-  
-  ## Description
-  
-  ${data.description}
-  
-  ## Table of Contents
-  
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  ${renderLicenseLink(data.license)}
-  
-  ## Installation
-  
-  ${data.installation}
-  
-  ## Usage
-  
-  ${data.usage}
-  
-  ## Countribution
-  
-  ${data.contribution}
-  
-  ## Tests
-  
-  ${data.tests}
-  
-  ${renderLicenseBadge(data.license)}
-  ${renderLicenseSection(data.license)}
-  
-  ## Questions
+${renderLicenseBadge(data.license)}
 
-  If you have any questions about the project, feel free to contact me:
+## Description
 
-  - GitHub: [${data.github}](https://github.com/${data.github})
-  - Email: ${data.email}
-      `;
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+${renderLicenseLink(data.license)}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+
+If you have any questions about the project, please feel free to contact me:
+
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+`;
 }
 
 module.exports = generateMarkdown;
